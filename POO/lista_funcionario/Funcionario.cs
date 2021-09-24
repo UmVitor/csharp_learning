52226c8c8d5fbc5b84a0109a08842f75337b9396
Vitor Barreto <vittor_barreto@hotmail.com>
2021-09-24 19:28:54 -0300

    listas update

--- POO/lista_funcionario/Funcionario.cs
+++ POO/lista_funcionario/Funcionario.cs
@@ -2,16 +2,20 @@ namespace lista_funcionario
 {
     public class Funcionario
     {
-        public int Id{get; private set;}
-        public string Nome {get; private set;}
+        public int Id{get; set;}
+        public string Nome {get; set;}
         public double Salario {get; set;}
 
-        public void Cadastro(int id, string nome, double salario){
+        public Funcionario(int id, string nome, double salario){
             Id = id;
             Nome = nome;
             Salario = salario;
         }
 
+        public void Percentage(double perc){
+            perc = perc/100;
+            Salario = Salario + Salario*perc;
+        }
 
     }
 }
--- POO/lista_funcionario/Program.cs
+++ POO/lista_funcionario/Program.cs
@@ -9,7 +9,31 @@ namespace lista_funcionario
         static void Main(string[] args)
         {
             List<Funcionario> list = new List<Funcionario>();
-            
+            Console.Write("How many employees will be registred? ");
+            // int n = int.Parse(Console.ReadLine());
+            // for(int i=0;i<n;i++){
+            //     Console.WriteLine("Employee #" + i+1 + ":");
+            //     Console.Write("Id: ") ;
+            //     int id = int.Parse(Console.ReadLine());
+            //     Console.Write("Name: ");
+            //     string name = Console.ReadLine();
+            //     Console.Write("Salary: ");
+            //     double salary = double.Parse(Console.ReadLine());
+            //     list.Add(new Funcionario(id,name,salary));
+            // }
+
+            list.AddRange( new Funcionario[] {  new Funcionario { id = 2, nome = "Frank", salario = 500.00 },
+                                                new Funcionario { id = 3, nome = "Bob", salario = 1300.00 },
+                                                new Funcionario { id = 4, nome = "Alex", salario = 2000.00 } } );
+
+          //  Console.Write("Enter the employee id that will have salary increase: ");
+           // int id_emp = int.Parse(Console.ReadLine());
+           // int search = list.Find(x => x.Id == id_emp);
+            // Console.WriteLine("Updated list of emplyees: ");
+            // foreach(Funcionario obj in list){
+            //     Console.WriteLine(obj.Id + ", " + obj.Nome
+            //                       + ", " + obj.Salario);
+            // }
         }
     }
 }
