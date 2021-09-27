a680ec8bc378063015e78afaf759f1c1b9a51a1c
Vitor Barreto <vittor_barreto@hotmail.com>
2021-09-26 22:04:37 -0300

    DateTime and TimeSpan

--- special_topics/Program.cs
+++ special_topics/Program.cs
@@ -29,9 +29,49 @@ namespace special_topics
             // Console.WriteLine(d8);
 
             //TimeSpan-----------------------------------------------
-            TimeSpan t1 = new TimeSpan(0,1,30);
-            Console.WriteLine(t1);
-            Console.WriteLine(t1.Ticks);
+            //Construtores
+            // TimeSpan t1 = new TimeSpan();
+            // TimeSpan t2 = new TimeSpan(900000000L); // ticks para horas
+            // TimeSpan t3 = new TimeSpan(2, 11, 21); // horas, minutos, segundos
+            // TimeSpan t4 = new TimeSpan(1, 2, 11, 21); // dias, horas, minutos, segundos
+            // TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321); // dias, horas, minutos, segundos, millissegundos
+            // Console.WriteLine(t1);
+            // Console.WriteLine(t2);
+            // Console.WriteLine(t3);
+            // Console.WriteLine(t4);
+            // Console.WriteLine(t5);
+
+            // // Utilizando o From conseguimos converter um valor para o formato TimeSpan
+            // TimeSpan t1 = TimeSpan.FromDays(1.5); // TimeSpan de um dia e meio
+            // TimeSpan t2 = TimeSpan.FromHours(1.5); // TimeSpan de uma hora e meia
+            // TimeSpan t3 = TimeSpan.FromMinutes(1.5); //TimeSpan de um minuto e meio
+            // TimeSpan t4 = TimeSpan.FromSeconds(1.5); //TimeSpan de um segundo e meio
+            // TimeSpan t5 = TimeSpan.FromMilliseconds(1.5);
+            // TimeSpan t6 = TimeSpan.FromTicks(900000000L);
+            // Console.WriteLine(t1);
+            // Console.WriteLine(t2);
+            // Console.WriteLine(t3);
+            // Console.WriteLine(t4);
+            // Console.WriteLine(t5);
+            // Console.WriteLine(t6);
+
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime()); // Converte o DateTime para Local
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime()); // Converte o DateTime para UTC
+            Console.WriteLine();
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d3: " + d3);
+            Console.WriteLine("d3 Kind: " + d3.Kind);
+            Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+            Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
 
 
         }
