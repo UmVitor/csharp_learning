d134a67daae394e1c1fbfb3006e3e4ba0505d03c
Vitor Barreto <vittor_barreto@hotmail.com>
2021-09-30 13:46:06 -0300

    Heranca Account

---
+++ POO/HerancaAccount/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HerancaAccount.Entities;
+
+namespace HerancaAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown",100.0,500.00);
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
