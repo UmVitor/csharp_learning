b4906e33a3f258b775644217cbea51282f14c46c
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-03 18:29:58 -0300

    Directiories create and read

---
+++ special_topics/FileDirectory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace FileDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp1\myfolder";
+            try{
+                var folders = Directory.EnumerateDirectories(path, "*.*",SearchOption.AllDirectories); // LIstar as pastas no diretório
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                Directory.CreateDirectory(@"c:\temp1\myfolder\newfolder"); 
+            }
+            catch (IOException e){
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
