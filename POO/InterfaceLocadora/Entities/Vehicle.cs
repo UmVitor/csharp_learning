6efeb736c85bf864e96ee8227fbaa2fe7111ad6e
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-04 17:23:54 -0300

    Aluguel de carros sem intefaces

---
+++ POO/InterfaceLocadora/Entities/Vehicle.cs
@@ -0,0 +1,16 @@
+namespace InterfaceLocadora.Entities
+{
+    public class Vehicle
+    {
+        public string Model { get; set; }
+        
+        public Vehicle(string model) 
+        {
+            Model = model;
+               
+        }
+        
+        
+
+    }
+}
--- POO/InterfaceLocadora/Program.cs
+++ POO/InterfaceLocadora/Program.cs
@@ -1,27 +1,35 @@
 ﻿using System;
-using System.Globalization;
 using InterfaceLocadora.Entities;
+using System.Globalization;
+using InterfaceLocadora.Services;
+
 namespace InterfaceLocadora
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Enter retal data:");
-            System.Console.Write("Car Model: ");
-            string carModel = Console.ReadLine();
+            Console.WriteLine("Enter rental data");
+            System.Console.Write("Car model: ");
+            string model = Console.ReadLine();
             System.Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
-            DateTime t1 = DateTime.Parse(Console.ReadLine());
-            System.Console.Write("Return (dd/MM/yyyy hh:mm): ");
-            DateTime t2 = DateTime.Parse(Console.ReadLine());
-            System.Console.Write("Enter price per hour: ");
-            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            System.Console.Write("Enter price per day: ");
-            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            System.Console.Write("return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
             
-            Rent car = new Rent(carModel, t1, t2, pricePerHour, pricePerDay);
-            System.Console.WriteLine("INVOICE");
-            System.Console.WriteLine(car);
-        }   
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalServices rentalService = new RentalServices(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            System.Console.WriteLine("INVOICE:");
+            System.Console.WriteLine(carRental.Invoice);
+
+        }
     }
 }
