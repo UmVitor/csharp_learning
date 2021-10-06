236762734e0e451301b1d26b11ae27774e42bc6b
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-06 13:35:20 -0300

    Paypal interface

---
+++ POO/InterfaceContrato/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+namespace InterfaceContrato.Services
+{
+    public interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest( double amount, int months);
+    }
+}
