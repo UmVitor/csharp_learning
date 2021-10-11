95198a89af2595213e912c39594cbadccf6202cb
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-11 14:58:34 -0300

    dictionary first implementation

---
+++ POO/DictionaryEx1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            // atribuição do dictionary
+            //cookies[chave] = value
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "123127192";
+            cookies["phone"] = "567562343"; //sobrescreve o valor anterior, pois o Dict não admite repretições
+
+            Console.WriteLine(cookies["phone"]); // output -> 567562343
+
+            cookies.Remove("email"); // irá remover a chave email
+            //podemos testar se a chave email está presente no Dictionary, da seguinte forma
+
+            if(cookies.ContainsKey("email")){
+                Console.WriteLine(cookies["email"]);
+            }else{
+                Console.WriteLine("There is no 'email' key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+            //imprime todos os cookies
+            foreach(KeyValuePair<string, string> item in cookies){
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+            //ou poderiamos fazer desta forma
+            //foreach(var item in cookies){
+            //	Console.WriteLine(item.Key + ": " + item.Value);
+            //}
+        }
+    }
+}
---
+++ POO/DictionaryEx2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DictionaryEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter file full path: ");
+            string path = @"C://temp1/in.txt";//Console.ReadLine();
+            Dictionary<string, string> vote = new Dictionary<string, string>();
+            try {
+                using(StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(',');
+                        if(vote.ContainsKey(line[0])){
+                            vote[line[0]] = (int.Parse(vote[line[0]]) + int.Parse(line[1])).ToString();
+                        }else
+                        {
+                            vote[line[0]] = line[1];
+                        }
+                    }
+                    foreach (var item in vote)
+                    {
+                        System.Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
