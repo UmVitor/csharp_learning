afd02291b038805d65c38963396bb9aad50825fe
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-11 11:23:03 -0300

    Another example using hashes and override

---
+++ POO/HashSetAndHashSortEx4/Entities/Person.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+namespace HashSetAndHashSortEx4.Entities
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public Person(){}
+        public Person (string name, DateTime date) : this(){
+            Name = name;
+            Date = date;
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Person)){
+                return false;
+            }
+            Person other = obj as Person;
+            return Name.Equals(other.Name);
+        }
+    }
+}
---
+++ POO/HashSetAndHashSortEx4/Program.cs
@@ -0,0 +1,48 @@
+﻿// Um site de internet registra um log de acessos dos usuários. Um
+// registro de log consiste no nome de usuário e o instante em que o
+// usuário acessou o site no padrão ISO 8601, separados por espaço,
+// conforme exemplo. Fazer um programa que leia o log de acessos a
+// partir de um arquivo, e daí informe quantos usuários distintos
+// acessaram o site.
+using System;
+using System.IO;
+using System.Collections.Generic;
+using HashSetAndHashSortEx4.Entities;
+namespace HashSetAndHashSortEx4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.Write("Enter file full path: ");
+            string path = @"C:\\temp1\in.txt";//Console.ReadLine();
+            HashSet<Person> people = new HashSet<Person>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(" ");
+                        people.Add(new Person(line[0],DateTime.Parse(line[1])));
+                    }
+                    PrintCollection(people);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void PrintCollection<T>(IEnumerable<T> collection){
+            int count=0;
+            foreach (T obj in collection)
+            {
+                count++;
+            }
+            Console.WriteLine("Total Users: " + count);
+        }
+    }
+}
+
