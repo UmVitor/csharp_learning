3fc982130455d1af7393eedd991c184f61480348
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-11 15:31:16 -0300

    Extension methods

---
+++ POO/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ExtensionMethods.Extensions;
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 10, 6, 8, 10, 45);
+            System.Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
