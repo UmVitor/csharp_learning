e56dfe88bf40604b807eba4efa29b7cb7779575d
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-11 09:54:29 -0300

    Example with only HashSet

---
+++ POO/HashSetAndHashSortEx1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetAndHashSortEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("TV")); // Checa se em set contem "TV"
+
+            foreach(string p in set){ // Percorre e imprime o conjunto
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
