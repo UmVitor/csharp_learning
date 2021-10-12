4355f0309543ee494e742786c0be059e357ff81b
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-12 15:29:50 -0300

    Using delegates an lambda functions

--- POO/ComparisonEx1/Program.cs
+++ POO/ComparisonEx1/Program.cs
@@ -14,7 +14,7 @@ namespace ComparisonEx1
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = CompareProducts;
+            Comparison<Product> comp = (p1,p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()); 
 
             list.Sort(comp); //sor utilizando um delegate(referencia para a função com type safety)
 
@@ -23,8 +23,6 @@ namespace ComparisonEx1
                 System.Console.WriteLine(p);
             }
         }
-        static int CompareProducts(Product p1, Product p2){
-            return p1.Name.ToUpper().CompareTo(p2.Name);
-        }
+
     }
 }
