4af0825bbc4b6f491873183341f88524bac25a7f
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-14 15:01:02 -0300

    LINQ

---
+++ POO/LINQEx1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the data source
+            int[] numbers = new int[] {2, 3, 4, 5};
+
+            //Define query expressions
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x*10); // even numbers and multiply all numbers by 10
+
+            //we can turn resulto into a list with ToList
+            //result.ToList();
+
+            //execute the query
+            foreach(int x in result){
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ POO/LINQEx2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the data source
+            int[] numbers = new int[] {2, 3, 4, 5};
+
+            //Define query expressions
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x*10); // even numbers and multiply all numbers by 10
+
+            //execute the query
+            foreach(int x in result){
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
