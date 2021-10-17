4350ef5b3379327c3a282e58bce93ad0379bfdd7
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-17 17:59:58 -0300

    Initiate with ASP NET

--- POO/LINQEx4/Program.cs
+++ POO/LINQEx4/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// Fazer um programa para ler os dados (nome, email e salário)
+// de funcionários a partir de um arquivo em formato .csv.
+// Em seguida mostrar, em ordem alfabética, o email dos
+// funcionários cujo salário seja superior a um dado valor
+// fornecido pelo usuário.
+// Mostrar também a soma dos salários dos funcionários cujo
+// nome começa com a letra 'M'.
+// Maria,maria @gmail.com,3200.00
+// Alex, alex @gmail.com,1900.00
+// Marco, marco @gmail.com,1700.00
+// Bob, bob @gmail.com,3500.00
+// Anna, anna @gmail.com,2800.00
+
+using System;
 using System.Linq;
 using System.Globalization;
 using System.Collections.Generic;
